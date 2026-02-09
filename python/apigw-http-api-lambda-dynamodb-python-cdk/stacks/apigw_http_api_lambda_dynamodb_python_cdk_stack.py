656e97469dd4418c4c646b8dfda3dee2ac94b5b2
jaykrupa-gh3 <jaykrupa+gh3@amazon.com>
2026-02-09 15:45:20 -0500

    Add comprehensive logging: VPC Flow Logs, API Gateway access logs, CloudTrail, DynamoDB PITR and streams

--- python/apigw-http-api-lambda-dynamodb-python-cdk/stacks/apigw_http_api_lambda_dynamodb_python_cdk_stack.py
+++ python/apigw-http-api-lambda-dynamodb-python-cdk/stacks/apigw_http_api_lambda_dynamodb_python_cdk_stack.py
@@ -9,7 +9,11 @@ from aws_cdk import (
     aws_apigateway as apigw_,
     aws_ec2 as ec2,
     aws_iam as iam,
+    aws_logs as logs,
+    aws_s3 as s3,
+    aws_cloudtrail as cloudtrail,
     Duration,
+    RemovalPolicy,
 )
 from constructs import Construct
 
@@ -19,6 +23,37 @@ TABLE_NAME = "demo_table"
 class ApigwHttpApiLambdaDynamodbPythonCdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        # S3 bucket for centralized log storage
+        log_archive_bucket = s3.Bucket(
+            self,
+            "LogArchiveBucket",
+            encryption=s3.BucketEncryption.S3_MANAGED,
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+            enforce_ssl=True,
+            lifecycle_rules=[
+                s3.LifecycleRule(
+                    transitions=[
+                        s3.Transition(
+                            storage_class=s3.StorageClass.GLACIER,
+                            transition_after=Duration.days(90),
+                        )
+                    ],
+                    expiration=Duration.days(2555),
+                )
+            ],
+            removal_policy=RemovalPolicy.RETAIN,
+        )
+
+        # CloudTrail for API activity logging
+        trail = cloudtrail.Trail(
+            self,
+            "CloudTrail",
+            is_multi_region_trail=True,
+            include_global_service_events=True,
+            management_events=cloudtrail.ReadWriteType.ALL,
+            bucket=log_archive_bucket,
+        )
 
         # VPC
         vpc = ec2.Vpc(
@@ -32,6 +67,20 @@ class ApigwHttpApiLambdaDynamodbPythonCdkStack(Stack):
                 )
             ],
         )
+
+        # VPC Flow Logs
+        vpc_flow_log_group = logs.LogGroup(
+            self,
+            "VpcFlowLogGroup",
+            retention=logs.RetentionDays.ONE_MONTH,
+            removal_policy=RemovalPolicy.DESTROY,
+        )
+
+        vpc.add_flow_log(
+            "FlowLog",
+            destination=ec2.FlowLogDestination.to_cloud_watch_logs(vpc_flow_log_group),
+            traffic_type=ec2.FlowLogTrafficType.ALL,
+        )
         
         # Create VPC endpoint
         dynamo_db_endpoint = ec2.GatewayVpcEndpoint(
@@ -43,28 +92,41 @@ class ApigwHttpApiLambdaDynamodbPythonCdkStack(Stack):
 
         # This allows to customize the endpoint policy
         dynamo_db_endpoint.add_to_policy(
-            iam.PolicyStatement(  # Restrict to listing and describing tables
+            iam.PolicyStatement(
                 principals=[iam.AnyPrincipal()],
-                actions=[                "dynamodb:DescribeStream",
-                "dynamodb:DescribeTable",
-                "dynamodb:Get*",
-                "dynamodb:Query",
-                "dynamodb:Scan",
-                "dynamodb:CreateTable",
-                "dynamodb:Delete*",
-                "dynamodb:Update*",
-                "dynamodb:PutItem"],
+                actions=[
+                    "dynamodb:DescribeStream",
+                    "dynamodb:DescribeTable",
+                    "dynamodb:Get*",
+                    "dynamodb:Query",
+                    "dynamodb:Scan",
+                    "dynamodb:CreateTable",
+                    "dynamodb:Delete*",
+                    "dynamodb:Update*",
+                    "dynamodb:PutItem"
+                ],
                 resources=["*"],
             )
         )
 
-        # Create DynamoDb Table
+        # Create DynamoDb Table with point-in-time recovery and streams
         demo_table = dynamodb_.Table(
             self,
             TABLE_NAME,
             partition_key=dynamodb_.Attribute(
                 name="id", type=dynamodb_.AttributeType.STRING
             ),
+            point_in_time_recovery=True,
+            stream=dynamodb_.StreamViewType.NEW_AND_OLD_IMAGES,
+        )
+
+        # Lambda CloudWatch Logs with retention
+        lambda_log_group = logs.LogGroup(
+            self,
+            "LambdaLogGroup",
+            log_group_name="/aws/lambda/apigw_handler",
+            retention=logs.RetentionDays.ONE_MONTH,
+            removal_policy=RemovalPolicy.DESTROY,
         )
 
         # Create the Lambda function to receive the request
@@ -82,18 +144,40 @@ class ApigwHttpApiLambdaDynamodbPythonCdkStack(Stack):
             memory_size=1024,
             timeout=Duration.minutes(5),
             tracing=lambda_.Tracing.ACTIVE,
+            log_group=lambda_log_group,
         )
 
         # grant permission to lambda to write to demo table
         demo_table.grant_write_data(api_hanlder)
         api_hanlder.add_environment("TABLE_NAME", demo_table.table_name)
 
-        # Create API Gateway
+        # API Gateway Access Logs
+        api_log_group = logs.LogGroup(
+            self,
+            "ApiAccessLogGroup",
+            retention=logs.RetentionDays.ONE_MONTH,
+            removal_policy=RemovalPolicy.DESTROY,
+        )
+
+        # Create API Gateway with access logging
         apigw_.LambdaRestApi(
             self,
             "Endpoint",
             handler=api_hanlder,
+            cloud_watch_role=True,
             deploy_options=apigw_.StageOptions(
                 tracing_enabled=True,
+                access_log_destination=apigw_.LogGroupLogDestination(api_log_group),
+                access_log_format=apigw_.AccessLogFormat.json_with_standard_fields(
+                    caller=True,
+                    http_method=True,
+                    ip=True,
+                    protocol=True,
+                    request_time=True,
+                    resource_path=True,
+                    response_length=True,
+                    status=True,
+                    user=True,
+                ),
             ),
         )
